00e3c60ed217e3cd8b21dbbbc430e01f39d1562b
Jackw <wingatejack2021@gmail.com>
2025-05-28 14:41:56 +0100

    Update import path for Footer component in GreatBritishRailways page to reflect new directory structure.

--- src/pages/project-sub-pages/GreatBritishRailways.jsx
+++ src/pages/project-sub-pages/GreatBritishRailways.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from '../components/Footer';
+import Footer from '../../components/mainfunctions/Footer';
 import { Helmet } from 'react-helmet-async';
 
 export default function GreatBritishRailways() {
